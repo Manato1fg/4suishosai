945fd6bd2784bbfc2ad839710a3702dc8f7c8295
Manato1fg <manato1fg@gmail.com>
2019-03-09 13:09:19 +0900

    No message available

--- SuiranStamp/js/suirango.js
+++ SuiranStamp/js/suirango.js
@@ -10,11 +10,26 @@ async function onSuccessLoading(stream){
     
     const model = await tf.loadLayersModel('https://raw.githubusercontent.com/Manato2cc/4suishosai/master/SuiranStamp/python/model/model.json');
     const worker = new Worker("js/worker.js")
+    // メッセージを受信してコンソールに表示する
+    worker.addEventListener('message', (message) => {
+        console.log(JSON.parse(message.data));
+    });  
+      
     setInterval(function(){
-        worker.postMessage("");
+        worker.postMessage(JSON.stringify(getImage(document.getElementById("video"))));
     }, 100);
 }
 
 function onError(err){
     alert("正常にカメラが起動できませんでした。\nerror code: "+err.code);
+}
+
+
+function getImage(video){
+    var canvas = document.createElement('canvas');
+    canvas.height = video.height;
+    canvas.width = video.width;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    return ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
--- SuiranStamp/js/worker.js
+++ SuiranStamp/js/worker.js
@@ -1,19 +1,10 @@
 importScripts("https://unpkg.com/@tensorflow/tfjs");
 importScripts("https://unpkg.com/@tensorflow-models/mobilenet");
+
 addEventListener("message", function(e){
-    var image = tf.browser.fromPixels(getImage(document.getElementById("video")), 3);  // for example
+    var image = tf.fromPixels(JSON.parse(e.data));
     var axis = 0;
     image = image.expandDims(axis);
     const prediction = model.predict(image);
-    console.log(prediction); 
+    self.postMessage(JSON.stringify(prediction));
 })
-
-function getImage(video){
-    var canvas = document.createElement('canvas');
-    canvas.height = video.height;
-    canvas.width = video.width;
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    return ctx.getImageData(0, 0, canvas.width, canvas.height);
-}
-
