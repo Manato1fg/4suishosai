ce3299e1bf5180e139593d4a1a9ac225ad37a432
Manato1fg <manato1fg@gmail.com>
2019-03-09 13:12:58 +0900

    NMA
    (No message available)

--- SuiranStamp/js/worker.js
+++ SuiranStamp/js/worker.js
@@ -2,9 +2,13 @@ importScripts("https://unpkg.com/@tensorflow/tfjs");
 importScripts("https://unpkg.com/@tensorflow-models/mobilenet");
 
 addEventListener("message", function(e){
+    console.log(e);
+    
     var image = tf.fromPixels(JSON.parse(e.data));
     var axis = 0;
     image = image.expandDims(axis);
     const prediction = model.predict(image);
+    console.log(prediction);
+    
     self.postMessage(JSON.stringify(prediction));
 })
